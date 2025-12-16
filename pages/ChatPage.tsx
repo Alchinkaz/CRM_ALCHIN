d20466e4373ff30c1d13b33b416ba7c52f4b626d
Alchinkaz <alchinkazakhstan@gmail.com>
2025-12-16 19:12:53 +0500

    feat: Improve chat UI and remove redundant icon
    Refactors the chat page to remove a manually defined `MessageSquare` icon, using the imported `lucide-react` version instead. Also, the background image style on the messages feed has been removed for a cleaner appearance.

--- pages/ChatPage.tsx
+++ pages/ChatPage.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useRef, useEffect } from 'react';
 import { User, ChatMessage } from '../types';
-import { Send, Image as ImageIcon, Search, ChevronLeft, MoreVertical, Phone, User as UserIcon, Users } from 'lucide-react';
+import { Send, Image as ImageIcon, Search, ChevronLeft, MoreVertical, Phone, User as UserIcon, Users, MessageSquare } from 'lucide-react';
 
 interface ChatPageProps {
   currentUser: User;
@@ -217,7 +217,7 @@ export const ChatPage: React.FC<ChatPageProps> = ({ currentUser, users, messages
                 </div>
 
                 {/* Messages Feed */}
-                <div className="flex-1 overflow-y-auto p-4 space-y-4 bg-[url('https://site-assets.fontawesome.com/releases/v6.5.1/svgs/solid/message-lines.svg')] bg-fixed bg-no-repeat bg-center bg-opacity-5 dark:bg-opacity-5">
+                <div className="flex-1 overflow-y-auto p-4 space-y-4">
                     {/* Date separators could go here */}
                     {activeMessages.map(msg => {
                         const isMe = msg.senderId === currentUser.id;
@@ -294,23 +294,3 @@ export const ChatPage: React.FC<ChatPageProps> = ({ currentUser, users, messages
     </div>
   );
 };
-
-// Icon helper
-function MessageSquare(props: any) {
-    return (
-        <svg 
-            {...props}
-            xmlns="http://www.w3.org/2000/svg" 
-            width="24" 
-            height="24" 
-            viewBox="0 0 24 24" 
-            fill="none" 
-            stroke="currentColor" 
-            strokeWidth="2" 
-            strokeLinecap="round" 
-            strokeLinejoin="round"
-        >
-            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-        </svg>
-    )
-}
